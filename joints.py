44d08a56241f790b61f2d806acff5e2b89c7fb6e
Pavel Tarasov <tarasradio@mail.ru>
2021-08-18 16:22:44 +0300

    Remove unused files - remove joints class; - remove old leg swing calculation; - remove test body swing program.

--- joints.py
+++
@@ -1,8 +0,0 @@
-class LegJoints:
-  def __init__(self):
-    self.hip_yaw = 0.0 # z (vertical) axis (bottom directed)
-    self.hip_roll = 0.0 # x (Longitudinal) axis (forward directed)
-    self.hip_pitch = 0.0 # y (Lateral) axis (right directed)
-    self.knee_pitch = 0.0 # y (Lateral) axis
-    self.ankle_roll = 0.0 # x (Longitudinal) axis
-    self.ankle_pitch = 0.0 # y (Lateral) axis
