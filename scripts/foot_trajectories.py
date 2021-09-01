bab8c83d033cdc08adbcc4800289fe68324524e5
tarasradio <tarasradio@mail.ru>
2021-09-01 21:39:33 +0300

    Add z-direction tracking in foot swing trajectory.

--- scripts/foot_trajectories.py
+++ scripts/foot_trajectories.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+from matplotlib import patches
 
 def foot_swing_trajectory(t0, xi, yi, xd, yd, swing_period, lifting_height = 0.1, raising_period = 0.1, lowering_period = 0.1):
   trajectory = { 't':[], 'x':[], 'y':[], 'z':[] }
@@ -32,20 +33,26 @@ def foot_swing_trajectory(t0, xi, yi, xd, yd, swing_period, lifting_height = 0.1
 
   return trajectory
 
-def foot_swing_trajectory_magid(t0, xi, yi, xd, yd, step_period, step_height = 0.05):
+def foot_swing_trajectory_magid(t0, xi, yi, zi, xd, yd, zd, step_period, step_height = 0.05):
   trajectory = { 't':[], 'x':[], 'y':[], 'z':[] }
 
   t = t0; dt = 0.01
-  x = xi; y = yi; z = 0
 
   step_length = xd - xi
   step_width = yd - yi
 
+  k = math.atan2(zd - zi, step_length)
+
   def next_position(t):
-    phase =  (t - t0) / (step_period)
-    z = 0.5 * step_height * (1 - math.cos(2 * math.pi * phase))
+    phase = (t - t0) / step_period
+    
     x = xi + 0.5 * step_length * (1 - math.cos(math.pi * phase))
     y = yi + 0.5 * step_width * (1 - math.cos(math.pi * phase))
+    z_h = k * (x - xi) + zi
+    z_u = 0.5 * step_height * (1 - math.cos(2 * math.pi * phase))
+    z = z_h + z_u
+
+    print('z_h = ', z_h)
 
     trajectory['t'].append(t)
     trajectory['x'].append(x)
@@ -73,3 +80,39 @@ def foot_support_trajectory(t0, xi, yi, zi, support_period):
     next_position(t + t0)
   
   return trajectory
+
+def test_foot_swing():
+  fig, ax = plt.subplots()
+  fig.canvas.set_window_title('Swing trajectory for stairs climbing.')
+
+  plt.title('Swing trajectory for stairs climbing.')
+  plt.xlabel('x (m)')
+  plt.ylabel('z (m)')
+
+  step_l = 0.3; step_h = 0.1
+  stair_h = 0.1
+
+  xi = 0.5; zi = 0.1
+
+  xd = xi + step_l
+  zd = zi + stair_h
+
+  swing1 = foot_swing_trajectory_magid(0, xi, 0, zi, xd, 0, zd, 0.8 * 2, step_h)
+  swing2 = foot_swing_trajectory_magid(0, xi + step_l, 0, zi + stair_h, xd + step_l, 0, zd + stair_h, 0.8 * 2, step_h)
+
+  first_stairs = patches.Rectangle((xi - step_l / 2, zi - stair_h), step_l, stair_h, linewidth=1, edgecolor='red', facecolor='none', lineStyle='--')
+  ax.add_patch(first_stairs)
+
+  second_stairs = patches.Rectangle((xd - step_l / 2, zd - stair_h), step_l, stair_h, linewidth=1, edgecolor='red', facecolor='none', lineStyle='--')
+  ax.add_patch(second_stairs)
+
+  fird_stairs = patches.Rectangle((xd + step_l - step_l / 2, zd + stair_h - stair_h), step_l, stair_h, linewidth=1, edgecolor='red', facecolor='none', lineStyle='--')
+  ax.add_patch(fird_stairs)
+
+  ax.plot(swing1['x'], swing1['z'])
+  ax.plot(swing2['x'], swing2['z'])
+  plt.axis("equal")
+
+  plt.show()
+
+test_foot_swing()
